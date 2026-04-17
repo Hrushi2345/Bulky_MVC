8e1a8c3c379aa999684115a6a5fe917cc0b227d0
Hrushikesh Bhadane <hrushikesh9970@gmail.com>
2026-04-18 02:57:57 +0530

    Category Create and Back btn

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,7 +7,9 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Category Name")]
         public String Name { get; set; }
+        [Display(Name = "Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
